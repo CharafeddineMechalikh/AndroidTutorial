42e6509e14b8c5458e840c1a9fe159643c4484ac
Charafeddine Mechalikh <46229052+CharafeddineMechalikh@users.noreply.github.com>
2023-04-15 13:43:02 +0100

    Update LoginActivityTest.java

--- app/src/androidTest/java/com/example/myapplication/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/myapplication/LoginActivityTest.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.view.View;
 
-import android.util.Log;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
@@ -30,40 +29,37 @@ public class LoginActivityTest {
         activityRule.getScenario().onActivity(activity -> decorView = activity.getWindow().getDecorView());
     }
 
-   @Test
-public void testCorrectCredentials() {
-    Log.i("TEST", "Starting testCorrectCredentials()");
-    Espresso.onView(withId(R.id.login_email)).perform(ViewActions.typeText("correct@example.com"));
-    Log.i("TEST", "Typed email");
-    Espresso.pressBack();
-    onView(withId(R.id.login_password)).perform(ViewActions.typeText("password"));
-    Log.i("TEST", "Typed password");
-    Espresso.pressBack();
-    onView(withId(R.id.login_button)).perform(ViewActions.click());
-    Log.i("TEST", "Clicked login button");
+    @Test
+    public void testCorrectCredentials() {
+        Espresso.onView(withId(R.id.login_email)).perform(ViewActions.typeText("correct@example.com"));
+        Espresso.pressBack();
+        onView(withId(R.id.login_password)).perform(ViewActions.typeText("password"));
+        Espresso.pressBack();
+        onView(withId(R.id.login_button)).perform(ViewActions.click());
 
-    // Check if user info is saved to shared preferences
-    try {
-        Thread.sleep(10000);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+        // Check if user info is saved to shared preferences
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Check if HomeActivity is launched after successful login
+        onView(withId(R.id.welcome_textview))
+                       .check(ViewAssertions.matches(isDisplayed()));
+
+        // Assert that the HomeActivity is started
+        ActivityScenario<HomeActivity> scenario = ActivityScenario.launch(HomeActivity.class);
+        scenario.onActivity(activity -> {
+            // Assert that the user information is saved to SharedPreferences
+            SharedPreferences preferences =  PreferenceManager.getDefaultSharedPreferences(activity);
+            assertEquals("correct", preferences.getString("family_name", ""));
+            assertEquals("example", preferences.getString("first_name", ""));
+            assertEquals("correct@example.com", preferences.getString("email", ""));
+            assertEquals(30, preferences.getInt("age", 0));
+            assertEquals("123 Main St", preferences.getString("address", ""));
+        });
     }
-    
-
-    // Assert that the HomeActivity is started
-    ActivityScenario<HomeActivity> scenario = ActivityScenario.launch(HomeActivity.class);
-    scenario.onActivity(activity -> {
-        Log.i("TEST", "Started HomeActivity");
-        // Assert that the user information is saved to SharedPreferences
-        SharedPreferences preferences =  PreferenceManager.getDefaultSharedPreferences(activity);
-        assertEquals("correct", preferences.getString("family_name", ""));
-        assertEquals("example", preferences.getString("first_name", ""));
-        assertEquals("correct@example.com", preferences.getString("email", ""));
-        assertEquals(30, preferences.getInt("age", 0));
-        assertEquals("123 Main St", preferences.getString("address", ""));
-    });
-}
-
 
     @Test
     public void testIncorrectCredentials() {
@@ -75,7 +71,7 @@ public void testCorrectCredentials() {
         onView(withId(R.id.login_button)).perform(ViewActions.click());
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
